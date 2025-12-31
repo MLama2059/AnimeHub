6345cf50d862ccfdbc6e02ec208ff56ca2c3b34a
MLama2059 <lamamanish2059@gmail.com>
2025-12-31 22:50:40 +0545

    Popover improvements

---
+++ AnimeHubClient/wwwroot/js/Position.js
@@ -0,0 +1,9 @@
+﻿window.checkIfPopoverWillFlip = (element) => {
+    if (!element) return false;
+    const rect = element.getBoundingClientRect();
+    const popoverWidth = 320; // 280px content + margins/padding
+    const availableSpaceOnRight = window.innerWidth - rect.right;
+
+    // Flip ONLY if the space on the right is less than the popover width
+    return availableSpaceOnRight < popoverWidth;
+};
